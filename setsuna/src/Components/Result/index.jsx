29edf9bb5220312cc628c183ef1e291b373eb326
坂本恭平 <Kyo27.gls@gmail.com>
2020-02-09 18:05:48 +0900

    create Feedback and Result

---
+++ setsuna/src/Components/Result/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './index.css';
+
+class Result extends React.Component{
+    render(){
+
+        return(
+            <React.Fragment>
+        　　<nav>
+　　　　　　　<div> 
+ 
+　　　　　　　<ul>
+                <li><a href="#">First</a></li>
+                <li><a href="#">Second</a></li>
+                <li><a href="#">Third</a></li>
+                <li><a href="#">Fourth</a></li>
+                <li><a href="#">Fifth</a></li>
+                <li><a href="#">Sixth</a></li>
+　　　　　　　</ul>
+　　　　　　　</div>
+　　　　　　　</nav>
+            </React.Fragment>
+
+        )
+    }
+}
+
+export default Result;
+
